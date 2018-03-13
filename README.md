6d119e69d0eb5492a426309ced438c80ee1d50b2
Josh Larson <joshlarson2015@gmail.com>
2018-03-13 14:46:25 -0500

    Added copyright, license, and readme

---
+++ README.md
@@ -0,0 +1,4 @@
+<a href='https://bintray.com/josh-larson/jlcommon/jlcommon?source=watch' alt='Get automatic notifications about new "jlcommon" versions'><img src='https://www.bintray.com/docs/images/bintray_badge_color.png'></a>
+[ ![Download](https://api.bintray.com/packages/josh-larson/jlcommon/jlcommon/images/download.svg) ](https://bintray.com/josh-larson/jlcommon/jlcommon/_latestVersion)
+
+This library serves as a way to speed up Java development using some standard threading, control, logging, and network classes.
--- src/main/java/me/joshlarson/jlcommon/control/Service.java
+++ src/main/java/me/joshlarson/jlcommon/control/Service.java
@@ -1,3 +1,26 @@
+/***********************************************************************************
+ * MIT License                                                                     *
+ *                                                                                 *
+ * Copyright (c) 2018 Josh Larson                                                  *
+ *                                                                                 *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy    *
+ * of this software and associated documentation files (the "Software"), to deal   *
+ * in the Software without restriction, including without limitation the rights    *
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell       *
+ * copies of the Software, and to permit persons to whom the Software is           *
+ * furnished to do so, subject to the following conditions:                        *
+ *                                                                                 *
+ * The above copyright notice and this permission notice shall be included in all  *
+ * copies or substantial portions of the Software.                                 *
+ *                                                                                 *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR      *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,        *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE     *
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER          *
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,   *
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE   *
+ * SOFTWARE.                                                                       *
+ ***********************************************************************************/
 package me.joshlarson.jlcommon.control;
 
 import javax.annotation.CheckForNull;
@@ -68,6 +91,7 @@ public abstract class Service {
 	 *
 	 * @param c        the class of intent to register for
 	 * @param consumer the consumer to run when the intent is fired
+	 * @param <T>      the type of intent to register for
 	 */
 	protected <T extends Intent> void registerForIntent(@Nonnull Class<T> c, @Nonnull Consumer<T> consumer) {
 		intentManager.registerForIntent(c, consumer);
@@ -78,6 +102,7 @@ public abstract class Service {
 	 *
 	 * @param c        the class of intent to unregister
 	 * @param consumer the consumer that was previous registered
+	 * @param <T>      the type of intent to unregister
 	 */
 	protected <T extends Intent> void unregisterForIntent(@Nonnull Class<T> c, @Nonnull Consumer<T> consumer) {
 		intentManager.unregisterForIntent(c, consumer);
