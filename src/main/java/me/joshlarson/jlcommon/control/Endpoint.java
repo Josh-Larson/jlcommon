74e39e847b5d3466161327610cb6bd867f7f14e7
Josh Larson <joshlarson2015@gmail.com>
2018-03-17 13:31:32 -0500

    Added utility functions to Endpoint to ease starting, stopping, and running

--- src/main/java/me/joshlarson/jlcommon/control/Endpoint.java
+++ src/main/java/me/joshlarson/jlcommon/control/Endpoint.java
@@ -23,6 +23,11 @@
  ***********************************************************************************/
 package me.joshlarson.jlcommon.control;
 
+import me.joshlarson.jlcommon.concurrency.Delay;
+import me.joshlarson.jlcommon.log.Log;
+
+import java.util.Collection;
+
 public abstract class Endpoint extends Manager {
 	
 	public final boolean startEndpoint() {
@@ -32,6 +37,103 @@ public abstract class Endpoint extends Manager {
 	public final void stopEndpoint() {
 		stop();
 		terminate();
+	}
+	
+	/**
+	 * Starts, runs, and then gracefully stops all endpoints within the collection
+	 *
+	 * @param endpoints the collection of endpoints
+	 */
+	public static void startRunStop(Collection<Endpoint> endpoints) {
+		if (start(endpoints))
+			run(endpoints);
+		stop(endpoints);
+	}
+	
+	/**
+	 * Starts, runs, and then gracefully stops all endpoints within the collection
+	 *
+	 * @param endpoints         the collection of endpoints
+	 * @param periodicSleepTime the time between isOperational checks during the run phase
+	 */
+	public static void startRunStop(Collection<Endpoint> endpoints, long periodicSleepTime) {
+		if (start(endpoints))
+			run(endpoints, periodicSleepTime);
+		stop(endpoints);
+	}
+	
+	/**
+	 * Attempts to start each of the endpoints in the collection
+	 *
+	 * @param endpoints the collection of endpoints
+	 * @return TRUE if each endpoint was successfully started, FALSE otherwise
+	 */
+	public static boolean start(Collection<Endpoint> endpoints) {
+		Log.i("Starting...");
+		for (Endpoint e : endpoints) {
+			try {
+				if (!e.startEndpoint()) {
+					Log.e("Failed to start endpoint: %s", e.getClass().getName());
+					return false;
+				}
+			} catch (Throwable t) {
+				Log.e("Caught exception during start. Endpoint: %s", e.getClass().getName());
+				Log.e(t);
+				return false;
+			}
+		}
+		Log.i("Started.");
+		return true;
+	}
+	
+	/**
+	 * Runs each of the endpoints in the collection with the default periodicSleepTime of 100ms
+	 *
+	 * @param endpoints the collection of endpoints
+	 */
+	public static void run(Collection<Endpoint> endpoints) {
+		run(endpoints, 100);
+	}
+	
+	/**
+	 * Runs each of the endpoints in the collection with the specified periodicSleepTime
+	 *
+	 * @param endpoints         the collection of endpoints
+	 * @param periodicSleepTime the time to sleep between isOperational calls
+	 */
+	public static void run(Collection<Endpoint> endpoints, long periodicSleepTime) {
+		while (Delay.sleepMilli(periodicSleepTime)) {
+			for (Endpoint e : endpoints) {
+				try {
+					if (!e.isOperational()) {
+						Log.e("Endpoint '%s' is no longer operational.", e.getClass().getName());
+						return;
+					}
+				} catch (Throwable t) {
+					Log.e("Caught exception during isOperational. Endpoint: %s", e.getClass().getName());
+					Log.e(t);
+					return;
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Attempts to stop each of the endpoints in the collection
+	 *
+	 * @param endpoints the collection of endpoints
+	 */
+	public static void stop(Collection<Endpoint> endpoints) {
+		Log.i("Stopping...");
+		for (Endpoint e : endpoints) {
+			try {
+				e.stopEndpoint();
+			} catch (Throwable t) {
+				Log.e("Caught exception during stop. Endpoint: %s", e.getClass().getName());
+				Log.e(t);
+			}
+		}
+		Log.i("Stopped.");
 	}
 	
 }
